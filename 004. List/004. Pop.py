abdb48470850c8298814f735a3b68d89775fbe51
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-03-23 07:28:01 +0530

    Add pop functionality to list manipulation
    Implement pop() method to remove elements from a list.

---
+++ 004. List/004. Pop.py
@@ -0,0 +1,12 @@
+#pop() - Deletes the object based on the index value
+# Default --> Removes the last element
+
+list1 = [11,22,33,44]
+list1.pop(2)
+print(list1)
+list1.pop()
+print(list1)
+
+# output
+# [11, 22, 44]
+# [11, 22]
