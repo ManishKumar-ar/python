77394296df9f2f892db6859067ed97a130b50283
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-03-23 07:36:00 +0530

    Create 006. Sort.py

---
+++ 004. List/006. Sort.py
@@ -0,0 +1,21 @@
+A = ["Oranges", "Strawberry", "Mango"]
+A.sort()
+print(A)
+
+# output
+# ['Mango', 'Oranges', 'Strawberry']
+
+
+
+
+
+
+A = ["Oranges", "Strawberry", "Mango"]
+B = A.sort()
+print(A)
+print(B)
+
+
+# output
+# ['Mango', 'Oranges', 'Strawberry']
+# None                                  # -----because it sort the original one, and didn't assign to new one
