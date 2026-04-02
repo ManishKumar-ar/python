3dc9b6bca157056da9d2d334c903048be4679cbc
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-02 17:17:14 +0530

    negative slicing in tuple
    Demonstrate tuple slicing with various print statements.

---
+++ 005. Tuple/006. Slicing in tuple.py
@@ -0,0 +1,10 @@
+t = ("Mumbai", 10, "Python", 20,30,40,50,60)
+print(t[2:5])
+print(t[2:])
+print(t[-2:])                               # start from -2(means 50) and go to last
+print(t[:2])
+# output
+# ('Python', 20, 30)
+# ('Python', 20, 30, 40, 50, 60)
+# (50, 60)
+# ('Mumbai', 10)
