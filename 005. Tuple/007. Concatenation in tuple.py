6e6eba3e30025761138d1b72dfe000d110cf4a48
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-02 17:47:28 +0530

    Add examples of tuple concatenation
    Demonstrate tuple concatenation with mixed types.

---
+++ 005. Tuple/007. Concatenation in tuple.py
@@ -0,0 +1,16 @@
+tup1 = ("welcome", "to", "today's")
+tup2 = ("class", "on", "python")
+tup3 = tup2 + tup1 
+print(tup3)
+# output
+# ('class', 'on', 'python', 'welcome', 'to', "today's")
+
+
+
+#  //////////////////////// concatenation on tuple allow join of different elements also///////////
+tup1 = ("welcome", "to", "today's")
+tup2 = ("class", "on", "python",52222)
+tup3 = tup2 + tup1 
+print(tup3)
+# output
+# ('class', 'on', 'python', 52222, 'welcome', 'to', "today's")
