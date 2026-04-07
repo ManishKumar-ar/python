d1b5379f5749b6279d6961ee57df5c4ada1da1b0
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-07 17:09:57 +0530

    Add car_info dictionary and print keys and values

---
+++ 007. Dictionary/003. Keys method.py
@@ -0,0 +1,11 @@
+car_info = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(car_info.keys())
+print(car_info.values())
+##############OUTPUT##################################
+# dict_keys(['brand', 'model', 'year'])
+# dict_values(['Ford', 'Mustang', 1964])
