186707a8b7b892f0b336fd7dcaaecdb340644cea
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-08 12:20:38 +0530

    Update and rename 002. map with lambda.py to 003. map with lambda.py

--- 009. Map/002. map with lambda.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-l = [1,2,3,4,5]
-
-newl = reduce(lambda x, y:x+y, l)
-print(newl)
-
-
-#### output #################
-# 15
---
+++ 009. Map/003. map with lambda.py
@@ -0,0 +1,9 @@
+lst = [1, 2, 3, 4, 5]
+
+newl = list(map(lambda x: x*x*x, lst))
+
+print(newl)
+
+
+#### output #################
+# [1, 8, 27, 64, 125]
