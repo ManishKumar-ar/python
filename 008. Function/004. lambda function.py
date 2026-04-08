25383c87f9505f7e70c133107dce38f28bd41a44
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-08 09:53:51 +0530

    Add lambda function to check even or odd numbers

---
+++ 008. Function/004. lambda function.py
@@ -0,0 +1,7 @@
+# Create a lambda function that checks a number is even or odd
+
+num = lambda x:"this is even number" if x %2==0 else "odd number"
+print(num(25))
+
+###output###############
+# odd number
