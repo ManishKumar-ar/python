b040c85cf2e185e354827524033c4c55bd450f12
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2026-04-10 22:15:04 +0530

    Add method to read the first line of a file

--- 010. File handling/001. Reading a file.py
+++ 010. File handling/001. Reading a file.py
@@ -15,3 +15,14 @@ print(file.read())
 # #####outout #########
 # this is manish prajapati
 # i am a new teacher here
+
+
+
+
+
+
+############ method -  3  ################################
+file = open("manish.txt", "r")
+print(file.readline())                    # but read only the first line of the file
+# #####outout #########
+# this is manish prajapati
